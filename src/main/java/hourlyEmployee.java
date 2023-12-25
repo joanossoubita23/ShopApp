940b69f710e25bbbe73dae2b425eda0e3436cc07
joanossoubita23 <joanossoubita@yahoo.fr>
2023-12-24 21:37:22 -0500

    m2

--- src/main/java/hourlyEmployee.java
+++ src/main/java/hourlyEmployee.java
@@ -1,2 +1,27 @@
-public class hourlyEmployee {
+public class hourlyEmployee extends  employee{
+    private  int hourWork;
+
+    public int getHourWork() {
+        return hourWork;
+    }
+
+    @Override
+    public String toString() {
+        return "hourlyEmployee{" +
+                "hourWork=" + hourWork +
+                "} " + super.toString();
+    }
+
+    public void setHourWork(int hourWork) {
+        this.hourWork = hourWork;
+    }
+
+    public hourlyEmployee(String name, String email, long salary, int id, int hourWork) {
+        super(name, email, salary, id);
+        this.hourWork = hourWork;
+    }
+
+    public hourlyEmployee(String name, String email, long salary, int id) {
+        super(name, email, salary, id);
+    }
 }
